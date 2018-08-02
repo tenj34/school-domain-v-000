42d0d8e59be0a9db5618ea706223147e23e24b4e
Juan Tenezaca <tenj34@gmail.com>
2018-08-02 15:51:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,31 @@
 class School
+
+# create a class School that can be initialized with a name
   def initialize(name)
     @name = name
-    @roster = {}
+    @roster = {} # a school must have a roster. Should be empty upon initialization
   end
 
   def roster
-    @roster
+    @roster # initializationze
   end
 
-  def add_student(name,grade)
+# should be able to add a student by calling add_student
+# giving it an argument of name and their grade 
+  def add_student(name,grade) 
     if @roster[grade] != nil
       @roster[grade] << name
     else
       @roster[grade] = [name]
     end
   end
-
+  
+# method grade should take an argument of a grade and return students of tht grade
   def grade(grade)
     @roster[grade]
   end
 
+# sort method of the student array alphabetically
   def sort
     @roster.each do |name,grade|
       grade.sort!
